90ea76f9545e00d1f27c5f91cdc469886b4aa9fe
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-03-24 18:03:45 +0100

    commented out firebase init

--- src/start.ts
+++ src/start.ts
@@ -8,27 +8,27 @@ import { ErrorCode } from './enums/error-code';
 
 const app = express();
 
-const firebaseApp = admin.initializeApp({
-	credential: applicationDefault(),
-});
-const db = firebaseApp.firestore();
+// const firebaseApp = admin.initializeApp({
+// 	credential: applicationDefault(),
+// });
+// const db = firebaseApp.firestore();
 
 const start = async () => {
 	// contractEventLoader(firebaseApp);
 	app.get('/', (req, res) => {
 		res.send('Its working');
 	});
-	app.get('/:tokenId', async (req, res) => {
-		const { tokenId } = req.params || {};
-		if (!tokenId)
-			return res.status(ErrorCode.BAD_REQUEST_400).send('Please provider the tokenId as param');
+	// app.get('/:tokenId', async (req, res) => {
+	// 	const { tokenId } = req.params || {};
+	// 	if (!tokenId)
+	// 		return res.status(ErrorCode.BAD_REQUEST_400).send('Please provider the tokenId as param');
 
-		// db.collection('tokens').doc(tokenId)
-		const docRef = await db.collection('tokens').doc(tokenId).get();
-		if (docRef.exists) res.json({ data: docRef.data(), id: docRef.id });
-		else
-			res.status(ErrorCode.NOT_FOUND_404).send(`The token with the id ${tokenId} doesn't exists.`);
-	});
+	// 	// db.collection('tokens').doc(tokenId)
+	// 	const docRef = await db.collection('tokens').doc(tokenId).get();
+	// 	if (docRef.exists) res.json({ data: docRef.data(), id: docRef.id });
+	// 	else
+	// 		res.status(ErrorCode.NOT_FOUND_404).send(`The token with the id ${tokenId} doesn't exists.`);
+	// });
 
 	app.listen(config.server.port, () =>
 		console.log(`The Server is listening to ${config.server.port}`),
