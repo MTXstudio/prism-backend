9d122244bdab0b446af0ac013ea69eb8628052b0
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-03-28 14:43:23 +0200

    implemented GET /tokens/:tokenId + GET /tokens + refactoring

--- src/events/loader.ts
+++ src/events/loader.ts
@@ -9,15 +9,13 @@ import {
 	tokenCreatedListener,
 } from './contract-event-listeners';
 import colors from 'colors';
-import { app } from 'firebase-admin';
+import { firestore } from 'firebase-admin';
 
 const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 const signer = provider.getSigner();
 const contract = new ethers.Contract(config.contract.address, abi, signer);
 
-export const contractEventLoader = async (firebaseApp: app.App) => {
-	const db = firebaseApp.firestore();
-
+export const contractEventLoader = async (db: firestore.Firestore) => {
 	// firestore.collection('yuser');
 	contract.on(
 		'Mint',
@@ -33,38 +31,38 @@ export const contractEventLoader = async (firebaseApp: app.App) => {
 		},
 	);
 
-	contract.on('ProjectCreated', (projectId: BigNumber, name: string) => {
-		console.log(colors.yellow('ProjectCreated'));
-		projectCreatedListener({ name, projectId }, db);
-	});
+	// contract.on('ProjectCreated', (projectId: BigNumber, name: string) => {
+	// 	console.log(colors.yellow('ProjectCreated'));
+	// 	projectCreatedListener({ name, projectId }, db);
+	// });
 
-	contract.on(
-		'CollectionCreated',
-		(name: string, collectionId: BigNumber, projectId: BigNumber, maxInvocation: BigNumber) => {
-			console.log(colors.red('CollectionCreated'));
-			collectionCreatedListener({ name, collectionId, projectId, maxInvocation }, db);
-		},
-	);
+	// contract.on(
+	// 	'CollectionCreated',
+	// 	(name: string, collectionId: BigNumber, projectId: BigNumber, maxInvocation: BigNumber) => {
+	// 		console.log(colors.red('CollectionCreated'));
+	// 		collectionCreatedListener({ name, collectionId, projectId, maxInvocation }, db);
+	// 	},
+	// );
 
-	contract.on(
-		'TokenCreated',
-		(
-			name: string,
-			tokenId: BigNumber,
-			projectId: BigNumber,
-			collectionId: BigNumber,
-			price: BigNumber,
-			maxSupply: BigNumber,
-		) => {
-			console.log(colors.cyan('TokenCreated'));
-			tokenCreatedListener({
-				collectionId,
-				maxSupply,
-				name,
-				price,
-				projectId,
-				tokenId,
-			});
-		},
-	);
+	// contract.on(
+	// 	'TokenCreated',
+	// 	(
+	// 		name: string,
+	// 		tokenId: BigNumber,
+	// 		projectId: BigNumber,
+	// 		collectionId: BigNumber,
+	// 		price: BigNumber,
+	// 		maxSupply: BigNumber,
+	// 	) => {
+	// 		console.log(colors.cyan('TokenCreated'));
+	// 		tokenCreatedListener({
+	// 			collectionId,
+	// 			maxSupply,
+	// 			name,
+	// 			price,
+	// 			projectId,
+	// 			tokenId,
+	// 		});
+	// 	},
+	// );
 };
--- src/start.ts
+++ src/start.ts
@@ -1,14 +1,15 @@
 import express from 'express';
-import admin from 'firebase-admin';
+import admin, { firestore } from 'firebase-admin';
 import { ErrorCode } from './enums/error-code';
 import { config } from './config/index';
-// import { contractEventLoader } from './events/loader';
 import './helpers/fetch-polyfill';
-// import { ErrorCode } from './enums/error-code';
+import { contractEventLoader } from './events/loader';
+import { json, urlencoded } from 'body-parser';
 
 const app = express();
 
-//https://stackoverflow.com/questions/41287108/deploying-firebase-app-with-service-account-to-heroku-environment-variables-wit
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 const firebaseApp = admin.initializeApp({
 	credential: admin.credential.cert({
@@ -20,30 +21,69 @@ const firebaseApp = admin.initializeApp({
 
 const db = firebaseApp.firestore();
 
-const start = async () => {
-	// contractEventLoader(firebaseApp);
-	db.collection('heroku').add({ isWorking: true });
-	app.get('/', (req, res) => {
-		res.send('Its working');
+app.get('/', (req, res) => {
+	res.send('Its working');
+});
+
+app.get('/tokens/:tokenId', async (req, res) => {
+	const { tokenId } = req.params || {};
+	if (!tokenId) return res.status(ErrorCode.BAD_REQUEST_400).send('Please provider params');
+
+	// db.collection('tokens').doc(tokenId)
+	const docRef = await db.collection('tokens').doc(tokenId).get();
+	if (docRef.exists) res.json({ data: docRef.data(), id: docRef.id });
+	else res.status(ErrorCode.NOT_FOUND_404).send(`The token with the id ${tokenId} doesn't exists.`);
+});
+
+app.get('/tokens', async (req, res) => {
+	const { traits } = req.body || {};
+	console.log('traits', traits);
+	if (!traits) return res.status(ErrorCode.BAD_REQUEST_400).send('Please provider params');
+
+	let snapshot;
+	try {
+		snapshot = await db
+			.collection('tokens')
+			.where(firestore.FieldPath.documentId(), 'in', traits)
+			.get();
+	} catch (e) {
+		console.error(e);
+		return res.send({
+			success: false,
+			message: `Failed to retrieve tokens with the ids ${traits}.`,
+		});
+	}
+
+	const docs: any[] = [];
+	snapshot.docs[0].data();
+	snapshot.forEach((doc) => {
+		docs.push(doc.data());
 	});
-	app.get('/:tokenId', async (req, res) => {
-		const { tokenId } = req.params || {};
-		if (!tokenId)
-			return res.status(ErrorCode.BAD_REQUEST_400).send('Please provider the tokenId as param');
 
-		// db.collection('tokens').doc(tokenId)
-		const docRef = await db.collection('tokens').doc(tokenId).get();
-		if (docRef.exists) res.json({ data: docRef.data(), id: docRef.id });
-		else
-			res.status(ErrorCode.NOT_FOUND_404).send(`The token with the id ${tokenId} doesn't exists.`);
-	});
+	res.send(docs);
+});
 
-	app.listen(config.server.port, () =>
-		console.log(`The Server is listening to ${config.server.port}`),
-	);
-};
+app.post('/token', async (req, res) => {
+	// db.collection('tokens').doc(tokenId)
+	let docRef;
+	try {
+		docRef = await db.collection('tokens').add({
+			name: (Math.random() + 1).toString(36).substring(7),
+		});
+	} catch (e) {
+		return res.send({ success: false, message: 'Failed to add a token' });
+	}
 
-start();
+	const doc = await docRef?.get();
+
+	res.json({ success: true, data: doc?.data() || {} });
+});
+
+contractEventLoader(db);
+
+app.listen(config.server.port, () =>
+	console.log(`The Server is listening to ${config.server.port}`),
+);
 
 // const tbl = await connectToTableLand();
 // if (!tbl) process.exit(-1);
