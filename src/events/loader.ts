d98a688b71c069dc011d9b3caff1db8a0f61919c
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-06-08 16:53:36 +0200

    refactoring

--- src/events/loader.ts
+++ src/events/loader.ts
@@ -1,9 +1,12 @@
 import { config } from '../config/index';
 import {
 	collectionCreatedListener,
+	collectionEditListener,
 	masterEditListener,
 	projectCreatedListener,
+	projectEditListener,
 	tokenCreatedListener,
+	tokenEditListener,
 } from './event-listeners';
 
 export const contractEventLoader = async () => {
@@ -54,38 +57,8 @@ export const contractEventLoader = async () => {
 	// 	},
 	// );
 
-	// config.contract.token.on('MasterEdit', collectionEditListener);
-
-	// config.contract.token.on(
-	// 	'ProjectEdit',
-	// 	(id: BigNumber, name: string, chef: string, traitTypes: string[]) => {
-	// 		console.log(`Project edit with follwing args: ${name}, ${id}, ${chef}, ${traitTypes}`);
-	// 	},
-	// );
-
-	// // config.contract.token.on('MasterEdit', collectionEditListener);
-
-	// config.contract.token.on(
-	// 	'CollectionEdit',
-	// 	(
-	// 		name: string,
-	// 		id: BigNumber,
-	// 		projectId: BigNumber,
-	// 		royalties: BigNumber,
-	// 		manager: string,
-	// 		maxInvocation: BigNumber,
-	// 		assetType: AssetType,
-	// 		paused: boolean,
-	// 	) => {
-	// 		console.log(
-	// 			`Collection edit with follwing args: ${name}, ${id}, ${projectId}, ${royalties}, ${manager}, ${maxInvocation}, ${assetType}, ${paused}`,
-	// 		);
-	// 	},
-	// );
-
+	config.contract.project.on('ProjectEdit', projectEditListener);
+	config.contract.project.on('CollectionEdit', collectionEditListener);
+	config.contract.token.on('TokenEdit', tokenEditListener);
 	config.contract.token.on('MasterEdit', masterEditListener);
-
-	// config.contract.token.on('MasterEdit', (masterId: BigNumber, traits: BigNumber[]) => {
-	// 	console.log(`Edit Master with follwing args: ${masterId}, ${traits}`);
-	// });
 };
