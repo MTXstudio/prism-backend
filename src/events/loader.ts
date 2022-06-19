fbf6560159e80d423e6be032478ddceb8946bd32
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-06-19 13:02:31 +0200

    removed unused comments

--- src/events/loader.ts
+++ src/events/loader.ts
@@ -11,52 +11,8 @@ import {
 
 export const contractEventLoader = async () => {
 	config.contract.project.on('ProjectCreated', projectCreatedListener);
-	// config.contract.project.on(
-	// 	'ProjectCreated',
-	// 	(id: BigNumber, name: string, owner: string, traitTypes: string[]) => {
-	// 		console.log(`Create Project with following args: ${id}, ${name}, ${owner}, ${traitTypes}`);
-	// 	},
-	// );
-
 	config.contract.project.on('CollectionCreated', collectionCreatedListener);
-	// config.contract.project.on(
-	// 	'CollectionCreated',
-	// 	(
-	// 		name: string,
-	// 		id: BigNumber,
-	// 		projectId: BigNumber,
-	// 		royalties: BigNumber,
-	// 		manager: string,
-	// 		maxInvocation: BigNumber,
-	// 		assetType: AssetType,
-	// 		paused: boolean,
-	// 	) => {
-	// 		console.log(
-	// 			`Create Collection with following args: ${id}, ${name}, ${projectId}, ${royalties}, ${manager}, ${maxInvocation}, ${assetType}, ${paused}}`,
-	// 		);
-	// 	},
-	// );
-
 	config.contract.token.on('TokenCreated', tokenCreatedListener);
-	// config.contract.token.on(
-	// 	'TokenCreated',
-	// 	(
-	// 		name: string,
-	// 		id: BigNumber,
-	// 		projectId: BigNumber,
-	// 		collectionId: BigNumber,
-	// 		priceInWei: BigNumber,
-	// 		maxSupply: BigNumber,
-	// 		traitType: string,
-	// 		assetType: AssetType,
-	// 		paused: boolean,
-	// 	) => {
-	// 		console.log(
-	// 			`Create Token with following args: ${id}, ${name}, ${projectId}, ${collectionId}, ${priceInWei}, ${maxSupply}, ${traitType}, ${assetType}, ${paused}}`,
-	// 		);
-	// 	},
-	// );
-
 	config.contract.project.on('ProjectEdit', projectEditListener);
 	config.contract.project.on('CollectionEdit', collectionEditListener);
 	config.contract.token.on('TokenEdit', tokenEditListener);
